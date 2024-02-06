130beaffc49045bb6bd2c7a93cb93fe5db07cca3
ReDMallard <marco@titori.it>
2024-02-06 04:36:47 +0100

    crea componenti event e connessioni per tag

---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $events = [
+            [
+                'name' => 'evento 1',
+                'date' => '2024-08-08',
+                'available_tickets' => 500
+            ],
+            [
+                'name' => 'evento 2',
+                'date' => '2024-03-13',
+                'available_tickets' => 501
+            ],
+            [
+                'name' => 'evento 3',
+                'date' => '2024-01-26',
+                'available_tickets' => 502
+            ],
+            [
+                'name' => 'evento 4',
+                'date' => '2024-03-18',
+                'available_tickets' => 10
+            ]
+        ];
+
+        foreach ($events as $event) {
+
+            $newEvent = new Event();
+            $newEvent->fill($event);
+            $newEvent->save();
+        }
+    }
+}
